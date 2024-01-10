575bde6f53c40be4ba356b80de04edc4a56df38b
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-01-10 23:02:56 +0900

    Update update.py

--- update.py
+++ update.py
@@ -5,6 +5,8 @@ from urllib import parse
 
 HEADER="""# 
 # 백준 & 프로그래머스 & SWEA 문제 풀이 목록
+[![Solved.ac
+프로필](http://mazassumnida.wtf/api/generate_badge?boj={handle})](https://solved.ac/{handle})
 """
 
 def main():
