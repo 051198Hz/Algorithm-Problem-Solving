e5b3f6e08e056652599a78c1b06f3035557620b9
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-01-11 03:42:12 +0900

    [Silver IV] Title: 비트 우정지수, Time: 16 ms, Memory: 2476 KB -BaekjoonHub

--- 백준/Silver/12782. 비트 우정지수/비트 우정지수.cc
+++ 백준/Silver/12782. 비트 우정지수/비트 우정지수.cc
@@ -2,10 +2,13 @@
 #include "string"
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int T; cin>>T;
     while (T-->0){
-        int*diff = new int[1000000]{0,};
         string a,b; cin>>a>>b;
+        int*diff = new int[a.size()]{0,};
         int cntP = 0, cntN = 0;
         for(int i = 0; i<a.size(); i++){
             diff[i] = (int)(a[i]-'0') - (int)(b[i]-'0');
