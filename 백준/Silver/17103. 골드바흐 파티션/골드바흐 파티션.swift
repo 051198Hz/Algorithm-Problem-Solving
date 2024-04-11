6632f147e576ad2e6ed7e4bbb1988931857a8b35
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-04-11 23:13:49 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 40 ms, Memory: 71644 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.swift
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.swift
@@ -1,13 +1,11 @@
 var isPrime = [Bool](repeating: true, count: 1_000_000 + 1)
+var primeArry = [Int]()
 isPrime[0] = false
 isPrime[1] = false
-for i in 2...Int(Double(1_000_000).squareRoot()+1){
-    if isPrime[i] {
-        var j = i*i
-        while j <= 1_000_000{
-            isPrime[j] = false
-            j += i
-        }
+for i in 2...1000000 where isPrime[i] == true{
+    primeArry.append(i)
+    for j in stride(from: i*i, through: 1_000_000, by: i){
+        isPrime[j] = false
     }
 }
 
@@ -17,12 +15,16 @@ for _ in 0..<times{
     let number = Int(readLine()!)!
 //    var visited = [Bool](repeating: false, count: 1_000_000+1)
     var cnt = 0
-    for i in stride(from: number, through: 0, by: -1){
-        if isPrime[i] && isPrime[number-i]{
-            cnt += 1
-        }else if(i <= number/2){
-            break
-        }
+    for prime in primeArry.reversed() where prime <= number && isPrime[number - prime] {
+        if number - prime > prime { break }
+        cnt+=1
     }
+//    for i in stride(from: number, through: 0, by: -1){
+//        if isPrime[i] && isPrime[number-i]{
+//            cnt += 1
+//        }else if(i <= number/2){
+//            break
+//        }
+//    }
     print(cnt)
 }
