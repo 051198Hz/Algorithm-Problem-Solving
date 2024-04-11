90176e3e661e96cc3a11b0eb85d37085a3ddf58a
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-04-12 00:29:33 +0900

    [Silver II] Title: 숫자 더하기, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/9440. 숫자 더하기/숫자 더하기.swift
+++ 백준/Silver/9440. 숫자 더하기/숫자 더하기.swift
@@ -1,10 +1,9 @@
-while(true){
-    var input = readLine()!.split(separator: " ")
-    if input.count == 1 { break }
+var input = readLine()!.split(separator: " ")
+while(input.count != 1){
     let zeroCnt = [String](repeating:"0", count:input.filter{ $0 == "0" }.count )
     var numbers = input.filter{ $0 != "0"}.map{String($0)}
     numbers = numbers[1..<numbers.count].map{String($0)}.sorted()
-    var n1 = [String]() //제일 작은 수
+    var n1 = [String]()
     var n2 = [String]()
     numbers.insert(contentsOf: zeroCnt , at: 2)
     for i in 0..<numbers.count{
@@ -16,4 +15,5 @@ while(true){
     }
     let answer = Int(n1.joined())! + Int(n2.joined())!
     print(answer)
+    input = readLine()!.split(separator: " ")
 }
