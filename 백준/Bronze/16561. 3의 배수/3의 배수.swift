d48fee1148fb323eee087185ca2cac12855c9647
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-04-12 03:41:40 +0900

    [Bronze II] Title: 3의 배수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/16561. 3의 배수/3의 배수.swift
+++ 백준/Bronze/16561. 3의 배수/3의 배수.swift
@@ -1,14 +1,9 @@
 func solution(n: Int)->Int{
-    let n = n/3
-    if n == 1 {return 0}
-    if n == 2 {return 0}
+    if n == 1 || n == 2 {return 0}
     var answer = 0
     for i in 1...n-2{
         answer += i
     }
     return answer
 }
-
-let number = Int(readLine()!)!
-let result = solution(n: number)
-print(result)
+print(solution(n: Int(readLine()!)!/3))
