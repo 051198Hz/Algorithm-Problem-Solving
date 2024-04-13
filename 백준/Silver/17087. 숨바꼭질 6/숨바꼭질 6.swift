3ae194d1da347d813bafe060837d40c6c5c93930
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-04-14 01:30:12 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 76 ms, Memory: 87680 KB -BaekjoonHub

---
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.swift
@@ -0,0 +1,14 @@
+import Foundation
+func GCD(a: Int, b: Int) -> Int{
+    var a = a, b = b
+    while(b != 0){
+        let n = a%b
+        a = b
+        b = n
+    }
+    return a
+}
+let ns = readLine()!.split(separator: " ").map{ Int(String($0))!}
+let distances = readLine()!.split(separator: " ").map{Int(String($0))!}.map{ abs($0 - ns[1]) }
+let answer = distances.reduce(distances.first! ){ GCD(a: $0, b: $1) }
+print(answer)
