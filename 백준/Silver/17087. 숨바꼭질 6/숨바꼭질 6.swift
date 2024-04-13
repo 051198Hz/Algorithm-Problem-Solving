1e5dc79167b44fa948b1598d6e62767e67b8ae36
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-04-14 01:34:38 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 68 ms, Memory: 77272 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.swift
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.swift
@@ -1,4 +1,3 @@
-import Foundation
 func GCD(a: Int, b: Int) -> Int{
     var a = a, b = b
     while(b != 0){
@@ -9,6 +8,6 @@ func GCD(a: Int, b: Int) -> Int{
     return a
 }
 let ns = readLine()!.split(separator: " ").map{ Int(String($0))!}
-let distances = readLine()!.split(separator: " ").map{Int(String($0))!}.map{ abs($0 - ns[1]) }
+let distances = readLine()!.split(separator: " ").map{ abs(Int(String($0))! - ns[1]) }
 let answer = distances.reduce(distances.first! ){ GCD(a: $0, b: $1) }
 print(answer)
