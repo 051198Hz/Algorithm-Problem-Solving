b46b23f3bff52c1e9a981ca88936dc6305921097
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-04-20 13:32:28 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 12 ms, Memory: 79520 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.swift
@@ -0,0 +1,123 @@
+import Foundation
+
+// MARK: - FileIO
+
+func stringToAscii(_ str: String) -> Int {
+    str.map { $0.asciiValue! }.map { Int($0) }.reduce(0) {$0 + $1}
+}
+
+struct FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private mutating func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) mutating func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    mutating func readIntArray(_ K: Int) -> [Int] {
+        var array = [Int]()
+        
+        for _ in 0..<K {
+            array.append(readInt())
+        }
+        
+        return array
+    }
+    
+    @inline(__always) mutating func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) mutating func readLine() -> String {
+        var now = read()
+        
+        while now == 10 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) mutating func readString() -> Int {
+        var str = 0
+        var now = read()
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        while now != 10 && now != 32 && now != 0 {
+            str += Int(now)
+            now = read()
+        }
+        return str
+    }
+    
+    mutating func readStringArray(_ K: Int) -> [String] {
+        var array = [String]()
+        
+        for _ in 0..<K {
+            array.append(readString())
+        }
+        
+        return array
+    }
+    
+    @inline(__always) mutating func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+var file = FileIO()
+var answer: [Character] = []
+for _ in 0..<file.readInt(){
+    if answer.isEmpty {
+        answer = file.readLine().map{ $0 }
+        continue
+    }
+    for (i,c) in file.readLine().enumerated(){
+        if answer[i] != c{
+            answer[i] = "?"
+        }
+    }
+}
+print(String(answer))
