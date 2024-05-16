6a4b4e7eb64bef34e7b9d84e007e04b5c4320abd
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-05-16 23:27:23 +0900

    [Silver V] Title: 소트인사이드, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.swift
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.swift
@@ -1,4 +1,1 @@
-var numbers = [Int]()
-readLine()!.map{ numbers.append(Int(String($0))!) }
-numbers.sort{ $0 > $1 }
-numbers.forEach{ print($0,terminator: "")}
+print(String(readLine()!.sorted(by:>)))
