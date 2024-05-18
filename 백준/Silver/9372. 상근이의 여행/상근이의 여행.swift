36030c78c99ff57a62e1aa0de46ffe60ea4049e1
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-05-18 23:12:27 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.swift
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.swift
@@ -2,7 +2,7 @@ let number = Int(readLine()!)!
 for _ in 0..<number{
     let cityPlane = readLine()!.split{ $0 == " " }.map{ Int($0)! }
     for _ in 0..<cityPlane[1]{
-        _ = readLine()!.split{ $0 == " " }.map{ Int($0)! }
+        _ = readLine()!
     }
     print(cityPlane[0] - 1)
 }
