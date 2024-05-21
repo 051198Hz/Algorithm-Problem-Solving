bfd50051649073e09260bf3468a0afaad97c575e
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-05-21 20:49:15 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 128 ms, Memory: 87388 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
@@ -1,19 +1,22 @@
 import Foundation
 //int인 from, to, through를 String으로 바꾼 풀이
 let maxPlate = Int(readLine()!)!
-var count = 0
-var answer = ""
-Hanoi(biggerPlate: maxPlate, from: "1", to: "3", through: "2")
-print(count)
-print(answer)
-func Hanoi(biggerPlate: Int, from: String, to: String, through: String){
-    if biggerPlate == 1{
+var hanoi = Hanoi()
+hanoi.getMoveWithCount(biggerPlate: maxPlate, from: "1", to: "3", through: "2")
+print(hanoi.count)
+print(hanoi.answer)
+struct Hanoi{
+    var count = 0
+    var answer = ""
+    mutating func getMoveWithCount(biggerPlate: Int, from: String, to: String, through: String){
+        if biggerPlate == 1{
+            answer += "\(from) \(to)\n"
+            count += 1
+            return
+        }
+        getMoveWithCount(biggerPlate: biggerPlate - 1, from: from, to: through, through: to)
         answer += "\(from) \(to)\n"
         count += 1
-        return
+        getMoveWithCount(biggerPlate: biggerPlate - 1, from: through, to: to, through: from)
     }
-    Hanoi(biggerPlate: biggerPlate - 1, from: from, to: through, through: to)
-    answer += "\(from) \(to)\n"
-    count += 1
-    Hanoi(biggerPlate: biggerPlate - 1, from: through, to: to, through: from)
 }
