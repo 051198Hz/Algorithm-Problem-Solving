2db7b248b4e7ca41cdae5ad107724a69a769ef40
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-05-21 20:16:27 +0900

    [Gold V] Title: 하노이 탑, Time: 304 ms, Memory: 88160 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.swift
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.swift
@@ -2,13 +2,17 @@ import Foundation
 let maxPlate = Int(readLine()!)!
 var count = pow(2, maxPlate)-1
 print(count)
-if maxPlate <= 20 { Hanoi(biggerPlate: maxPlate, from: 1, to: 3, through: 2) }
+var answer = ""
+if maxPlate <= 20 {
+    Hanoi(biggerPlate: maxPlate, from: 1, to: 3, through: 2)
+    print(answer)
+}
 func Hanoi(biggerPlate: Int, from: Int, to: Int, through: Int){
     if biggerPlate == 1{
-        print(from, to)
+        answer += "\(from) \(to)\n"
         return
     }
     Hanoi(biggerPlate: biggerPlate - 1, from: from, to: through, through: to)
-    print(from, to)
+    answer += "\(from) \(to)\n"
     Hanoi(biggerPlate: biggerPlate - 1, from: through, to: to, through: from)
 }
