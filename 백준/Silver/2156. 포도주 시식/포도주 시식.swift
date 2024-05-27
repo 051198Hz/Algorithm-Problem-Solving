d7251b0a19bd49222647eddbd1c98401e1180916
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-05-27 22:59:56 +0900

    [Silver I] Title: 포도주 시식, Time: 16 ms, Memory: 79672 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.swift
@@ -0,0 +1,16 @@
+import Foundation
+let length = Int(readLine()!)!
+var dp = [Int](repeating: 0, count: 10000+1)
+var stairs = [Int](repeating: 0, count: 10000+1)
+for i in 1...length{
+    stairs[i] = Int(readLine()!)!
+}
+dp[1] = stairs[1]
+dp[2] = dp[1] + stairs[2]
+if length > 2{
+    for i in 3...length{
+        dp[i] = max( stairs[i] + stairs[i-1] + dp[i-3], stairs[i] + dp[i-2] )
+        dp[i] = max( dp[i] , dp[i-1])
+    }
+}
+print(dp[length])
