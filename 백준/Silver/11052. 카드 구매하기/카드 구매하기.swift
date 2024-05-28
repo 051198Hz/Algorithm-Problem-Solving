e27fcbfeeb0db8266e8322cdddf8414213f3a5da
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-05-28 23:20:01 +0900

    [Silver I] Title: 카드 구매하기, Time: 20 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.swift
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.swift
@@ -1,25 +1,9 @@
-import Foundation
 let targetNumber = Int(readLine()!)!
-let packs = [0] + (readLine()!.split{ $0 == " " }.map{ Int($0)! } )
+let packs = (readLine()!.split{ $0 == " " }.map{ Int($0)! } )
 var dp = [Int](repeating: 0, count: targetNumber+1)
-dp[1] = packs[1]
-//카드를 1개 살 때 최대값
-//카드를 2개 살 때 최대값
-//...
-//카드를 n개 살 때 최대값
-
-// 카드를 1개 살 때 최댓값
-// = 카드팩 0개살 때 최댓값 + 1개짜리 카드팩, 1개짜리 카드팩
-// 카드를 2개 살 때 최댓값
-// = 카드팩 0개살 때 최댓값 + 2개짜리 카드팩, 카드 1개살 때 최댓값 + 1개짜리 카드팩
-// 카드를 3개 살 때 최댓값
-// = 카드 0개살 때 최댓값 + 3개짜리 카드팩, 카드팩 1개살 때 최댓값 + 2개짜리 카드팩, 카드팩 2개살 때 최댓값 + 1개짜리 카드팩
-// p[i] = max(dp[i], dp[j] + packs[i-j])
-if targetNumber > 1{
-    for i in packs.indices where i > 1{
-        for j in 0..<i{
-            dp[i] = max(dp[i], dp[j] + packs[i-j])
-        }
+for i in 1...targetNumber{
+    for j in 0..<i{
+        dp[i] = max(dp[i], dp[i-j-1] + packs[j])
     }
 }
 print(dp[targetNumber])
