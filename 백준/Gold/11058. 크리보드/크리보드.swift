935cc11ed7210800edf21826e9df283cb02267bc
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-06-03 23:54:33 +0900

    [Gold V] Title: 크리보드, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.swift
+++ 백준/Gold/11058. 크리보드/크리보드.swift
@@ -4,15 +4,10 @@ var dp = [Int](repeating: 0, count: n+1)
 for i in 1...n{
     dp[i] = i
     for j in 0...i-1{
-        if i - j - 3 > 0{ //전복붙붙붙...
-            dp[i] = max(dp[i], dp[j]+dp[j]*(i-j-2))
-        }
-        else if i - j - 3 == 0{ //전복붙
-            dp[i] = max(dp[i], dp[j]*2)
-        }
-        else{ //이전출력+a
-            dp[i] = max(dp[i], dp[j] + i-j)
-        }
+        //전복붙붙붙...
+        if i - j - 3 > 0{ dp[i] = max(dp[i], dp[j]+dp[j]*(i-j-2)) }
+        //전복붙
+        else if i - j - 3 == 0{ dp[i] = max(dp[i], dp[j]*2) }
     }
 }
 print(dp[n])
