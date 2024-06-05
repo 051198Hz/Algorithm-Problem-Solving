edaa127ed5fc9f9284a37b643b0f11cb6c308caa
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-06-05 21:45:41 +0900

    [Bronze V] Title: 몇개고?, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/27294. 몇개고？/몇개고？.swift
+++ 백준/Bronze/27294. 몇개고？/몇개고？.swift
@@ -1,22 +1,8 @@
-enum Eat{
-    case morning
-    case launch
-    case dinner
-}
 let td = readLine()!.split{ $0 == " " }.map{ Int($0)! }
-let t = td[0]
-let drink = td[1]
-var eat = Eat.launch
-var answer = 280
-if t <= 11{
-    eat = .morning
-}else if t > 16{
-    eat = .dinner
-}
-if drink == 1 || eat != .launch {
+let t = td[0] <= 11 || td[0] > 16 ? true : false
+let drink = td[1] == 1 ? true : false
+var answer = 320
+if drink == true || t != false {
     answer = 280
-}
-if eat == .launch && drink == 0{
-    answer = 320
 }
 print(answer)
