979a9ea3c19a22ce3a18050802039193498d2a65
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-06-06 17:14:21 +0900

    [Gold V] Title: 합분해, Time: 192 ms, Memory: 69368 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.swift
@@ -0,0 +1,15 @@
+let nk = readLine()!.split{ $0 == " " }.map{ Int($0)! }
+let n = nk[0], k = nk[1]
+var dp = [[Int]](repeating: [Int](repeating:0,count: k+1), count: n+1)
+for i in 0...k{
+    dp[0][i] = 1
+}
+for i in 1...n{
+    for j in 1...k{
+        for t in 0...n{
+            dp[i][j] += dp[t][j-1] % 1000000000
+        }
+        dp[i][j] %= 1000000000
+    }
+}
+print(dp[n][k])
