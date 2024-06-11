1619a8d065a67fda963371e5496bc529c6e8cce1
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-06-11 16:20:17 +0900

    [goormlevel] Title: 장마, Time: 3.48 ms, Memory: 103.01 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/194982/1. 장마/장마.swift
+++ goormlevel/194982/1. 장마/장마.swift
@@ -1,20 +1,33 @@
-import Foundation
-let nm = readLine()!.split{ $0 == " " }.map{ Int($0)! }
-let n = nm[0], m = nm[1]
-//n 집개수 m 장마기간
-var heights = readLine()!.split{ $0 == " " }.map{ Int($0)! }
-var isRained: [Int] = []
-for i in 1...m{
-    let rangeRain = readLine()!.split{ $0 == " " }.map{ Int($0)! }
-    for i in rangeRain[0]...rangeRain[1]{
-        heights[i-1] += 1
-        isRained.append(i-1)
-    }
-    if i%3 == 0{
-        Set(isRained).forEach{
-            heights[$0] -= 1
+struct Rain{
+    var str = String()
+    let n: Int
+    let m: Int
+    //n 집개수 m 장마기간
+			var heights: [Int] = []
+			var isRained: Set<Int> = []
+    mutating func rainy(){
+			heights = readLine()!.split{ $0 == " "}.map{ Int(String($0))! }
+        for i in 1...m{
+            let rangeRain = readLine()!.split{ $0 == " "}.map{ Int(String($0))! }
+            for i in rangeRain[0]...rangeRain[1]{
+                heights[i-1] += 1
+                isRained.insert(i-1)
+            }
+            if i%3 == 0{
+                for i in isRained{
+                    heights[i] -= 1
+                }
+                isRained = Set<Int>()
+            }
         }
-        isRained = []
+        for i in 0..<n{  
+					str.write(String(heights[i]))
+					str.write(" ")
+				}
     }
 }
-heights.forEach{ print("\($0) ", terminator: "") }
+
+let nm = readLine()!.split{ $0 == " "}.map{ Int(String($0))! }
+var rain = Rain(n: nm[0], m: nm[1])
+rain.rainy()
+print(rain.str)
