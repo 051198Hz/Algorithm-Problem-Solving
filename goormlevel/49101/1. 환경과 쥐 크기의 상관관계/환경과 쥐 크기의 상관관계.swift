1d4e910027314a6b9c325b6b3ce90fd762f2be56
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-06-13 01:26:12 +0900

    [goormlevel] Title: 환경과 쥐 크기의 상관관계, Time: 0.65 ms, Memory: 104.01 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/49101/1. 환경과 쥐 크기의 상관관계/환경과 쥐 크기의 상관관계.swift
+++ goormlevel/49101/1. 환경과 쥐 크기의 상관관계/환경과 쥐 크기의 상관관계.swift
@@ -7,19 +7,12 @@ var minExValueB = 0
 var maxCountA = Int.min
 var maxCountB = Int.min
 
-readLine()!.split{$0 == " " }.map{ Int(String($0))! }.forEach{
-	// maxA = max(maxA, $0)
-	// minA = min(minA, $0)
-	ratCountA[$0] += 1
-}
-readLine()!.split{$0 == " " }.map{ Int(String($0))! }.forEach{ 
-	// maxB = max(maxB, $0)
-	// minB = min(minB, $0)
-	ratCountB[$0] += 1 
-}
+readLine()!.split{$0 == " " }.map{ Int(String($0))! }.forEach{ ratCountA[$0] += 1 }
+readLine()!.split{$0 == " " }.map{ Int(String($0))! }.forEach{ ratCountB[$0] += 1 }
+
 for i in 1...100_000{
-	let lowerBound = i-2 > 0 ? i-2 : 1
-	let upperBound = i+2 <= 100_000 ? i+2 : 100_000
+	let lowerBound = max( i-2, 0 )
+	let upperBound = min(i+2 ,100_000)
 	let rangeCountA = ratCountA[lowerBound...upperBound].reduce(0){$0 + $1}
 	let rangeCountB = ratCountB[lowerBound...upperBound].reduce(0){$0 + $1}
 	if rangeCountA > maxCountA{
