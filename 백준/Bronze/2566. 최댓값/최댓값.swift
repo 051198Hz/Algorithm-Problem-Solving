9c3be708889ff3a8e9049401fcb7c82040182c2a
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-06-17 18:38:50 +0900

    [Bronze III] Title: 최댓값, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.swift
+++ 백준/Bronze/2566. 최댓값/최댓값.swift
@@ -2,6 +2,7 @@ import Foundation
 
 var positions:[Int: (Int,Int)] = [:]
 var maxValue = Int.min
+var x = 0, y = 0
 for i in 1...9{
     var j = 0
     readLine()!.split{ $0 == " " }
@@ -9,10 +10,12 @@ for i in 1...9{
     .forEach{ 
         j += 1
         positions.updateValue((i,j), forKey: $0)
-        maxValue = max(maxValue, $0)
+        if $0 > maxValue{
+            maxValue = $0
+            x = i
+            y = j
+        }
     }
 }
-let posMaxValue = positions[maxValue]!
-let x = posMaxValue.0, y = posMaxValue.1
 print(maxValue)
 print(x,y)
