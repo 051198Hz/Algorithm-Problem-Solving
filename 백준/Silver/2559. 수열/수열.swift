07beeee13bb7caa9d5ee0e147641e1d586bc1ae4
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-10-24 22:33:37 +0900

    [Silver III] Title: 수열, Time: 36 ms, Memory: 86560 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.swift
+++ 백준/Silver/2559. 수열/수열.swift
@@ -4,11 +4,11 @@ let nk = readLine()!.split{ $0 == " " }.map{ Int(String($0))! }
 let n = nk[0], k = nk[1]
 
 var cSum = [0]
-let numbers: [Int] = readLine()!.split{ $0 == " " }.map{  Int(String($0))! }
-numbers.forEach{ cSum.append(cSum.last! + $0) }
+readLine()!.split{ $0 == " " }.forEach{ cSum.append(cSum.last! + Int(String($0))!) }
 
 var maxSum = Int.min
 
 let answer = (k..<cSum.count).map{ i in max(maxSum, cSum[i] - cSum[i-k]) }.max()!
 
 print(answer)
+
