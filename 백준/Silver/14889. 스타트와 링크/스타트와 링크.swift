1228f7df3bcf1a2fef4d87000fa78223029c053f
Yune gim <82448654+051198Hz@users.noreply.github.com>
2025-01-10 21:56:26 +0900

    [Silver I] Title: 스타트와 링크, Time: 256 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.swift
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 let n = Int(readLine()!)!
 let playerPower = (0..<n).map { _ in
     readLine()!.split { $0 == " "}.map{ Int(String($0))! }
@@ -7,7 +5,7 @@ let playerPower = (0..<n).map { _ in
 var pickedPlayer = [Bool](repeating: true, count: playerPower.count)
 var differ = Int.max
 
-func pick(_ idx: Int, _ count: Int) {
+func pick(_ idx: Int, _ count: Int) -> Bool {
     if count == pickedPlayer.count/2 {
         var currentPower = [0,0]
         for i in 0..<pickedPlayer.count {
@@ -21,24 +19,25 @@ func pick(_ idx: Int, _ count: Int) {
         }
         let curDiff = abs(currentPower[0] - currentPower[1])
         if curDiff == 0 {
-            print("0")
-            exit(0)
+            differ = 0
+            return true
         }
         if differ > curDiff {
             differ = curDiff
         }
-        return
+        return false
     }
     for i in idx..<pickedPlayer.count {
         if pickedPlayer[i] {
             pickedPlayer[i] = false
-            pick(i+1, count+1)
+            if pick(i+1, count+1) { return true }
             pickedPlayer[i] = true
         }
     }
+    return false
 }
 // 플레이어가 서로 속한 걸 어떻게 알지?
 // 이걸 굳이 구분하지 않고 점수에 넣는 방법은 없을까?
 // n/2명만 뽑으면 나머지는 알아서 넣어지니,, 그렇게 가보자
-pick(0, 0)
+_=pick(0, 0)
 print(differ)
