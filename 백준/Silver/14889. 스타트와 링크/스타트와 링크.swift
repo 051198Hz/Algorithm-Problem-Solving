c6622f8120266c32b4ed1d65597e572e4183cc76
Yune gim <82448654+051198Hz@users.noreply.github.com>
2025-01-10 21:48:44 +0900

    [Silver I] Title: 스타트와 링크, Time: 264 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.swift
@@ -0,0 +1,37 @@
+let n = Int(readLine()!)!
+let playerPower = (0..<n).map { _ in
+    readLine()!.split { $0 == " "}.map{ Int(String($0))! }
+}
+var pickedPlayer = [Bool](repeating: true, count: playerPower.count)
+var differ = Int.max
+
+func pick(_ idx: Int, _ count: Int) {
+    if count == pickedPlayer.count/2 {
+        var currentPower = [0,0]
+        for i in 0..<pickedPlayer.count {
+            for j in 0..<pickedPlayer.count {
+                if pickedPlayer[i] && pickedPlayer[j] {
+                    currentPower[0] += playerPower[i][j]
+                } else if !pickedPlayer[i] && !pickedPlayer[j] {
+                    currentPower[1] += playerPower[i][j]
+                }
+            }
+        }
+        if differ > abs(currentPower[0] - currentPower[1]) {
+            differ = abs(currentPower[0] - currentPower[1])
+        }
+        return
+    }
+    for i in idx..<pickedPlayer.count {
+        if pickedPlayer[i] {
+            pickedPlayer[i] = false
+            pick(i+1, count+1)
+            pickedPlayer[i] = true
+        }
+    }
+}
+// 플레이어가 서로 속한 걸 어떻게 알지?
+// 이걸 굳이 구분하지 않고 점수에 넣는 방법은 없을까?
+// n/2명만 뽑으면 나머지는 알아서 넣어지니,, 그렇게 가보자
+pick(0, 0)
+print(differ)
