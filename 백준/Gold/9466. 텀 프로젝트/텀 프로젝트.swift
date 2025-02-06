cb5f4d1ef5dbaf9917906f9d2838a5d6aa76ee7f
Yune gim <82448654+051198Hz@users.noreply.github.com>
2025-02-06 13:30:10 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1780 ms, Memory: 79956 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.swift
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.swift
@@ -1,25 +1,52 @@
-let t = Int(readLine()!)!
-
-func findCycle( _ start: Int, _ cur: Int, _ studentList: inout [Int], _ cnt: Int, _ isVisited: inout [Int], _ startVertex: inout [Int]) -> Int {
-    if isVisited[cur] != 0 {
-        guard startVertex[cur] == start else { return 0 }
-        return cnt - isVisited[cur]
+class Queue {
+    var queue = [Int?]()
+    var head = 0
+    var isEmpty: Bool {
+        queue.count-head == 0
     }
-    let next = studentList[cur]
-    isVisited[cur] = cnt
-    startVertex[cur] = start
-    return findCycle(start, next, &studentList, cnt+1, &isVisited, &startVertex)
+    var front: Int? {
+        isEmpty ? nil : queue[head]
+    }
+    func enqueue(_ element: Int) {
+        queue.append(element)
+    }
+    func dequeue() -> Int? {
+        if isEmpty { return nil }
+        let ret = queue[head]
+        queue[head] = nil
+        head += 1
+        return ret
+    }
 }
-
+let t = Int(readLine()!)!
+func topologySort(_ vertexList: [Int], _ inDegreeList: inout [Int]) -> Int {
+    var acyclicVertexCount = 0
+    let queue = Queue()
+    for i in inDegreeList.indices {
+        guard inDegreeList[i] == 0 else { continue }
+        queue.enqueue(i)
+    }
+    while !queue.isEmpty {
+        let cur = queue.dequeue()!
+        let next = vertexList[cur]
+        inDegreeList[next] -= 1
+        if inDegreeList[next] == 0 {
+            queue.enqueue(next)
+        }
+        acyclicVertexCount += 1
+    }
+    return acyclicVertexCount
+}
+var totalAnswer = ""
 (0..<t).forEach { _ in
     let n = Int(readLine()!)!
-    var answer = n
-    var studentList = readLine()!.split { $0 == " " }.map { Int(String($0))! - 1 }
-    var isVisited = [Int](repeating: 0, count: n)
-    var startVertex = [Int](repeating: 0, count: n)
-    for index in studentList.indices where isVisited[index] == 0 {
-        let coupledCount = findCycle(index, index, &studentList, 1, &isVisited, &startVertex)
-        answer -= coupledCount
+    var inDegree = [Int](repeating: 0, count: n)
+    var vertexList = [Int](repeating: 0, count: n)
+    readLine()!.split { $0 == " " }.map { Int(String($0))! - 1 }.enumerated().forEach { i, value in
+        vertexList[i] = value
+        inDegree[value] += 1
     }
-    print(answer)
+    let answer = topologySort(vertexList, &inDegree)
+    totalAnswer.write("\(answer)\n")
 }
+print(totalAnswer)
