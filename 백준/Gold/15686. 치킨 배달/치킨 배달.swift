1da112d7774fed2ce79381822d62985445c326b8
Yune gim <82448654+051198Hz@users.noreply.github.com>
2025-04-10 02:00:44 +0900

    [Gold V] Title: 치킨 배달, Time: 12 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.swift
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.swift
@@ -1,77 +1,44 @@
-let nm = readLine()!.split { $0 == " " }.map { Int(String($0))! },
-    n = nm[0],
-    m = nm[1]
-// 첫째 줄에 N(2 ≤ N ≤ 50)과 M(1 ≤ M ≤ 13)이 주어진다.
-// 집의 개수는 2N개를 넘지 않으며, 적어도 1개는 존재한다.
-// 치킨집의 개수는 M보다 크거나 같고, 13보다 작거나 같다.
-let grid = (0..<n).map { _ in readLine()!.split { $0 == " " }.map { Int(String($0))! }} //[[Int]]
-// 치킨거리 = |x2 - x1| + |y2 - y1|
-// 치킨집 중 최대 m개만 남겼을 때, 도시의 치킨 거리의 합을 최소로 하시오.
-// 각 집에서 가장 가까운 치킨집까지의 거리가 치킨 거리.
-// 모든 치킨 거리의 합이 도시의 치킨 거리
-// 치킨집을 하나씩 선택해서, k개까지 선택했을 때의 모든 경우 까지 최소 도시 치킨 거리를 갱신한다.
+let input = readLine()!.split { $0 == " " }.map { Int($0)! }
+let n = input[0], m = input[1]
 
-struct Point {
-    init(_ y: Int, _ x: Int) {
-        self.y = y
-        self.x = x
-    }
-    let y: Int
-    let x: Int
-    
-    static func - (lhs: Point, rhs: Point) -> Int {
-        abs(lhs.x - rhs.x) + abs(lhs.y - rhs.y)
-    }
-}
-
-var placeList = [Point]()
-var homeList = [Point]()
-// 치킨집의 위치를 등록한다.
+var house: [(Int, Int)] = []
+var chicken: [(Int, Int)] = []
 for y in 0..<n {
+    let graph = readLine()!.split { $0 == " " }.map { Int($0) }
     for x in 0..<n {
-        if grid[y][x] == 2 {
-            placeList.append(Point(y, x))
-            continue
+        if graph[x] == 1 {
+            house.append((y, x))
         }
-        if grid[y][x] == 1 {
-            homeList.append(Point(y, x))
-            continue
+        if graph[x] == 2 {
+            chicken.append((y, x))
         }
     }
 }
 
-var minTotalDistance = Int.max
-// 1...m까지 치킨집을 뽑고, minTotalDistance를 계속해서 갱신한다.
-var selected = [Bool](repeating: false, count: placeList.count)
+var result = Int.max
 
+dfs(i: 0, selected: [])
+print(result)
 
-func selectChickens(_ idx: Int, _ selectedCount: Int) {
-    if selectedCount == nm[1] {
-        // 최소값
-        minTotalDistance = min(minTotalDistance, clacDistance())
+func dfs(i: Int, selected: [(Int, Int)]) {
+    if selected.count == m {
+        result = min(result, getDistance(coord: selected))
         return
     }
-    if idx >= placeList.count { return }
-    selected[idx] = true
-    selectChickens(idx+1, selectedCount+1)
-    selected[idx] = false
-    selectChickens(idx+1, selectedCount)
-}
-
-func clacDistance() -> Int {
-    var totalDist = 0
-    for home in homeList {
-        var minDist = Int.max
-        for i in placeList.indices {
-            guard selected[i] else { continue }
-            let dist = placeList[i] - home
-            minDist = dist < minDist ? dist : minDist
-        }
-        totalDist += minDist
+    
+    for index in i..<chicken.count {
+        dfs(i: index + 1, selected: selected + [chicken[index]])
     }
-    return totalDist
 }
 
-selectChickens(0,0)
-
-print(minTotalDistance)
+func getDistance(coord: [(Int, Int)]) -> Int {
+    var distance = 0
+    for h in house {
+        var minDistance = Int.max
+        for c in coord {
+            minDistance = min(minDistance, abs(h.0 - c.0) + abs(h.1 - c.1))
+        }
+        distance += minDistance
+    }
+    return distance
+}
