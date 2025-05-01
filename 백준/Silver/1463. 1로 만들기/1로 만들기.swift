cfb981e4114ce533e59864b17d1613fc1f63b303
Yune gim <82448654+051198Hz@users.noreply.github.com>
2025-05-01 22:30:34 +0900

    [Silver III] Title: 1로 만들기, Time: 48 ms, Memory: 76916 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.swift
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -1,9 +1,18 @@
-import Foundation
-let number = Int(readLine()!)!
-var dp = [Int](repeating:0,count: Int(pow(10.0, 6.0))+1)
-for i in 2...(number > 1 ? number:2){
-    dp[i] = dp[i-1] + 1
-    if i%2 == 0 { dp[i] = min(dp[i],dp[i/2]+1) }
-    if i%3 == 0 { dp[i] = min(dp[i],dp[i/3]+1) }
+//X가 3으로 나누어 떨어지면, 3으로 나눈다.
+//X가 2로 나누어 떨어지면, 2로 나눈다.
+//1을 뺀다.
+let n = Int(readLine()!)!
+var dp = [Int](repeating: Int.max, count: n+1)
+dp[1] = 0
+for i in 1...n {
+    if i*3 <= n && ((i*3)%3 == 0) {
+        dp[i*3] = min(dp[i*3], dp[i]+1)
+    }
+    if i*2 <= n && ((i*2)%2 == 0) {
+        dp[i*2] = min(dp[i*2], dp[i]+1)
+    }
+    if i+1 <= n {
+        dp[i+1] = min(dp[i+1], dp[i]+1)
+    }
 }
-print(dp[number])
+print(dp[n])
