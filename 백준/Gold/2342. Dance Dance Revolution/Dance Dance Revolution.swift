853a8fa3d90868effe76cafc5417db08d9ea6e79
Yune gim <82448654+051198Hz@users.noreply.github.com>
2025-05-03 21:55:12 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 168 ms, Memory: 129916 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.swift
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.swift
@@ -1,41 +1,45 @@
-var cmd = readLine()!.split { $0 == " " }.map { Int(String($0))! }
-cmd.removeLast()
-let n = cmd.count
-let cost = [
-    //   0  1  2  3  4
-        [1, 2, 2, 2, 2],
-        [3, 1, 3, 4, 3],
-        [3, 3, 1, 3, 4],
-        [3, 4, 3, 1, 3],
-        [3, 3, 4, 3, 1]
-]
+func calcCost(before: Int, after: Int) -> Int {
+    if before == 0 {
+        return 2
+    } else if before == after {
+        return 1
+    } else if before + 2 == after || before - 2 == after {
+        return 4
+    } else {
+        return 3
+    }
+}
+
+// calcTotalCost(index, left, right): 현재 두 발의 위치가 left, right일 때 arr[index...]를 모두 밟는 비용
+func calcTotalCost(_ stage: Int, left: Int, right: Int) -> Int {
+    if stage == n { return 0 }
+    
+    var ret = dp[stage][left][right]
+    if ret != inf { return ret }
+    
+    let next = cmd[stage]
+    if right != next {  // 왼쪽 발을 움직임
+        ret = min(ret, calcCost(before: left, after: next) + calcTotalCost(stage+1, left: next, right: right))
+    }
+    if left != next {   // 오른쪽 발을 움직임
+        ret = min(ret, calcCost(before: right, after: next) + calcTotalCost(stage+1, left: left, right: next))
+    }
+    
+    dp[stage][left][right] = ret
+    
+    return ret
+}
 
 let inf = 987654321
-var dp = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: inf, count: 5), count: 5), count: n+1)
-dp[0][0][0] = 0;
+var cmd = readLine()!.split(separator: " ").map{ Int(String($0))! }.dropLast()
+let n = cmd.count
+var dp = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: inf, count: 5), count: 5), count: n)
+var result = inf
 
-var answer = inf
-for stage in stride(from: 1, through: n, by: 1){
-    let nextPos = cmd[stage-1]
-    if nextPos == 0 { break }
-    for left in 0..<5 {
-        for right in 0..<5 {
-            dp[stage][left][nextPos] = min(dp[stage][left][nextPos], dp[stage-1][left][right] + cost[right][nextPos])
-        }
-    }
-    for right in 0..<5 {
-        for left in 0..<5 {
-            dp[stage][nextPos][right] = min(dp[stage][nextPos][right], dp[stage-1][right][left] + cost[left][nextPos])
-        }
-    }
+if n == 0 {
+    result = 0
+} else {
+    result = 2 + min(calcTotalCost(1, left: cmd[0], right: 0), calcTotalCost(1, left: 0, right: cmd[0]))
 }
 
-for left in 0..<5 {
-    for right in 0..<5 {
-        if answer > dp[n][left][right] {
-            answer = dp[n][left][right]
-        }
-    }
-}
-
-print(answer)
+print(result)
