2f9e2f20a6d8ac15470b2c8a14f5e9dd14eab3d5
Yune gim <82448654+051198Hz@users.noreply.github.com>
2025-06-26 20:48:02 +0900

    [Silver I] Title: 쿼드트리, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.swift
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.swift
@@ -1,49 +1,28 @@
-import Foundation
-var wbPaperLength = Int(readLine()!)!
-var wbPaper = Array<[Bool]>()
-var answer = "("
-for _ in 0..<wbPaperLength{
-    wbPaper.append(readLine()!.map{ (String($0) as NSString).boolValue })
-}
-func compress(paperLength: Int, StartX: Int, StartY:Int, paper: Array<[Bool]>)->String{
-    var answer = ""
-    var wNumber = 0
-    for i in paper.indices where i >= StartY && i<StartY+paperLength{
-        for j in paper.indices where j >= StartX && j<StartX+paperLength && paper[i][j] == false{
-            wNumber += 1
+func isAllSame(fromRow: Int, fromCol: Int, size: Int) -> Bool {
+    let first = image[fromRow][fromCol]
+
+    for i in fromRow..<fromRow+size {
+        for j in fromCol..<fromCol+size {
+            if first != image[i][j] { return false }
         }
     }
-    if wNumber == paperLength*paperLength{ 
-        answer += "0"
-        return answer }
-    if wNumber == 0 {
-        answer += "1"
-        return answer
-    }
-    
-    var topLeft = compress(paperLength: paperLength/2, StartX: StartX, StartY: StartY, paper: paper)
-    if topLeft.count != 1{
-        topLeft = "(" + topLeft + ")"
-    }
-    var topRight = compress(paperLength: paperLength/2, StartX: StartX+(paperLength/2), StartY: StartY, paper: paper)
-    if topRight.count != 1{
-        topRight = "(" + topRight + ")"
-    }
-    var bottomLeft = compress(paperLength: paperLength/2, StartX: StartX, StartY: StartY+(paperLength/2), paper: paper)
-    if bottomLeft.count != 1{
-        bottomLeft = "(" + bottomLeft + ")"
-    }
-    var bottomRight = compress(paperLength: paperLength/2, StartX: StartX+(paperLength/2), StartY: StartY+(paperLength/2), paper: paper)
-    if bottomRight.count != 1{
-        bottomRight = "(" + bottomRight + ")"
-    }
-    
-    return topLeft+topRight+bottomLeft+bottomRight
+    return true
 }
-var wbNumber = compress(paperLength: wbPaperLength, StartX: 0, StartY: 0, paper: wbPaper)
-if wbNumber.count != 1{ wbNumber = "(" + wbNumber + ")" }
-print( wbNumber )
 
-func +(left: (Int,Int), right: (Int,Int)) -> (Int,Int) {
-    return (left.0 + right.0, left.1 + right.1)
+let n = Int(readLine()!)!
+let image = (0..<n).map { _ in readLine()!.map { String($0) }}
+
+func compress(fromRow: Int, fromCol: Int, size: Int) -> String {
+    if isAllSame(fromRow: fromRow, fromCol: fromCol, size: size) {
+        return image[fromRow][fromCol]
+    }
+    let compressed = compress(fromRow: fromRow, fromCol: fromCol, size: size/2) +
+                    compress(fromRow: fromRow, fromCol: fromCol + size/2, size: size/2) +
+                    compress(fromRow: fromRow + size/2, fromCol: fromCol, size: size/2) +
+                    compress(fromRow: fromRow + size/2, fromCol: fromCol + size/2, size: size/2)
+
+    return "(\(compressed))"
 }
+
+let result = compress(fromRow: 0, fromCol: 0, size: n)
+print(result)
