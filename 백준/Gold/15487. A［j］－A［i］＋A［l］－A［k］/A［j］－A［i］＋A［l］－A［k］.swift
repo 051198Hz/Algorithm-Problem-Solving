976c6b1ef1b918af06a923855e943b017fc1e08b
Yune gim <82448654+051198Hz@users.noreply.github.com>
2026-01-12 15:01:03 +0900

    [Gold IV] Title: A[j]-A[i]+A[l]-A[k], Time: 124 ms, Memory: 92976 KB -BaekjoonHub

--- 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.swift
+++ 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.swift
@@ -7,62 +7,37 @@ final class FastScanner {
     @inline(__always)
     func readInt() -> Int {
         var num = 0
-        var sign = 1
-
-        while data[idx] == 10 || data[idx] == 32 { idx += 1 } // skip space & newline
-        if data[idx] == 45 { sign = -1; idx += 1 }
-
+        while data[idx] == 10 || data[idx] == 32 { idx += 1 }
         while data[idx] >= 48 {
             num = num * 10 + Int(data[idx] - 48)
             idx += 1
         }
-        return num * sign
+        return num
     }
 }
 
 let scanner = FastScanner()
 let n = scanner.readInt()
 
-var arr = [Int](repeating: 0, count: n)
-for i in 0..<n {
-    arr[i] = scanner.readInt()
+let NEG_INF = -1_000_000_000_000
+
+var dp0 = NEG_INF
+var dp1 = NEG_INF
+var dp2 = NEG_INF
+var dp3 = NEG_INF
+
+for _ in 0..<n {
+    let v = scanner.readInt()
+
+    let newDp3 = max(dp3, dp2 + v)
+    let newDp2 = max(dp2, dp1 - v)
+    let newDp1 = max(dp1, dp0 + v)
+    let newDp0 = max(dp0, -v)
+
+    dp3 = newDp3
+    dp2 = newDp2
+    dp1 = newDp1
+    dp0 = newDp0
 }
 
-var leftBest = [Int](repeating: Int.min, count: n)
-var rightBest = [Int](repeating: Int.min, count: n)
-
-// (-A[i] + A[j]) + (-A[k] + A[l])
-// 최소 i를 찾는다.
-// 최대 l를 찾는다.
-
-// 왼쪽에서부터 최대차이
-var minLeft = arr[0]
-leftBest[0] = Int.min  // j=0에서는 i<j 불가능
-
-for j in 1..<n {
-    leftBest[j] = leftBest[j-1] > (arr[j] - minLeft) ? leftBest[j-1] : (arr[j] - minLeft)
-    // 방금 지나친 j는 다시 사용하지 않으므로, 다음의 j보다 이전이다.
-    // 따라서 과거가 된 j를 i로 취급해 최솟값으로 활용할 수 있다.
-    minLeft = minLeft < arr[j] ? minLeft : arr[j]
-}
-
-var maxRight = arr[n-1]
-rightBest[n-1] = Int.min  // j=0에서는 i<j 불가능
-
-
-// n >= 4
-// 오른쪽에서부터 최대차이
-for k in stride(from: n-2, through: 0, by: -1) {
-    rightBest[k] = rightBest[k+1] > (maxRight - arr[k]) ? rightBest[k+1] : (maxRight - arr[k])
-    maxRight = maxRight > arr[k] ? maxRight : arr[k]
-}
-
-var answer = Int.min
-
-for t in 0..<(n-1) {
-    guard leftBest[t] != Int.min else { continue }
-    guard rightBest[t+1] != Int.min else { continue }
-    answer = (leftBest[t] + rightBest[t+1]) > answer ? (leftBest[t] + rightBest[t+1]) : answer
-}
-
-print(answer)
+print(dp3)
