803b37af14256469101543c08d3dc1747f375871
Yune gim <82448654+051198Hz@users.noreply.github.com>
2026-01-12 14:56:43 +0900

    [Gold IV] Title: A[j]-A[i]+A[l]-A[k], Time: 144 ms, Memory: 109692 KB -BaekjoonHub

--- 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.swift
+++ 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.swift
@@ -1,7 +1,32 @@
 import Foundation
 
-let n = Int(readLine()!)!
-let arr = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+final class FastScanner {
+    private var data: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()) + [0]
+    private var idx: Int = 0
+
+    @inline(__always)
+    func readInt() -> Int {
+        var num = 0
+        var sign = 1
+
+        while data[idx] == 10 || data[idx] == 32 { idx += 1 } // skip space & newline
+        if data[idx] == 45 { sign = -1; idx += 1 }
+
+        while data[idx] >= 48 {
+            num = num * 10 + Int(data[idx] - 48)
+            idx += 1
+        }
+        return num * sign
+    }
+}
+
+let scanner = FastScanner()
+let n = scanner.readInt()
+
+var arr = [Int](repeating: 0, count: n)
+for i in 0..<n {
+    arr[i] = scanner.readInt()
+}
 
 var leftBest = [Int](repeating: Int.min, count: n)
 var rightBest = [Int](repeating: Int.min, count: n)
@@ -15,10 +40,10 @@ var minLeft = arr[0]
 leftBest[0] = Int.min  // j=0에서는 i<j 불가능
 
 for j in 1..<n {
-    leftBest[j] = max(leftBest[j-1], arr[j] - minLeft)
+    leftBest[j] = leftBest[j-1] > (arr[j] - minLeft) ? leftBest[j-1] : (arr[j] - minLeft)
     // 방금 지나친 j는 다시 사용하지 않으므로, 다음의 j보다 이전이다.
     // 따라서 과거가 된 j를 i로 취급해 최솟값으로 활용할 수 있다.
-    minLeft = min(minLeft, arr[j])
+    minLeft = minLeft < arr[j] ? minLeft : arr[j]
 }
 
 var maxRight = arr[n-1]
@@ -28,15 +53,16 @@ rightBest[n-1] = Int.min  // j=0에서는 i<j 불가능
 // n >= 4
 // 오른쪽에서부터 최대차이
 for k in stride(from: n-2, through: 0, by: -1) {
-    rightBest[k] = max(rightBest[k+1], maxRight - arr[k])
-    maxRight = max(maxRight, arr[k])
+    rightBest[k] = rightBest[k+1] > (maxRight - arr[k]) ? rightBest[k+1] : (maxRight - arr[k])
+    maxRight = maxRight > arr[k] ? maxRight : arr[k]
 }
 
 var answer = Int.min
 
 for t in 0..<(n-1) {
-    guard leftBest[t] != Int.min, rightBest[t+1] != Int.min else { continue }
-    answer = max(leftBest[t] + rightBest[t+1], answer)
+    guard leftBest[t] != Int.min else { continue }
+    guard rightBest[t+1] != Int.min else { continue }
+    answer = (leftBest[t] + rightBest[t+1]) > answer ? (leftBest[t] + rightBest[t+1]) : answer
 }
 
 print(answer)
